a70a8cfb3ae31c4628fafc09f834edbf7166f640
PhilippVidal <Philipp.Vidal.HN@gmail.com>
2023-01-20 12:48:56 +0100

    Add fix for punching through object when aiming at mounted explosive

---
+++ MoreExplosives/Scripts/4_World/MoreExplosives/Classes/Modded/DayZPlayerImplementMeleeCombat.c
@@ -0,0 +1,9 @@
+//Potential fix for players being able to punch/hit a placed explosive and clip their head through a base wall
+modded class DayZPlayerImplementMeleeCombat
+{
+	override void Init(DayZPlayerImplement player)
+	{
+		super.Init(player);
+		m_NonAlignableObjects.Insert(MOE_ExplosiveBase);
+	}
+}
