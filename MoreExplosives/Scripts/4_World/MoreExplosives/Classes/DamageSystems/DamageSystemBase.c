c3dc5f95563cf18e542962d342b385b2aaf951ed
PhilippVidal <Philipp.Vidal.HN@gmail.com>
2023-06-29 14:09:10 +0200

    Move general checks to base damage system class

--- MoreExplosives/Scripts/4_World/MoreExplosives/Classes/DamageSystems/DamageSystemBase.c
+++ MoreExplosives/Scripts/4_World/MoreExplosives/Classes/DamageSystems/DamageSystemBase.c
@@ -2,7 +2,33 @@ class MOE_DamageSystemBase
 {
 	bool CanDealDamage(MOE_ExplosiveBase explosive, MOE_ExplosionObject explosiveObject, vector explosionPosition, Object target, int component, string dmgZone, string ammo)
 	{
-		return target != null;
+		BaseBuildingBase baseBuildingBase;
+		if(!CastTo(baseBuildingBase,target))
+		{
+			return false;
+		}
+		
+		if(!super.CanDealDamage(explosive, explosiveObject, explosionPosition, target, component, dmgZone, ammo))
+		{
+			return false;
+		}
+		
+		if(explosive.CanOnlyDamagePlacementTarget() && (target != explosive.GetPlacementTarget()))
+		{
+			return false;
+		}
+		
+		if(GetMOESettings().IsRaidSchedulingEnabled && !GetMOE().GetRaidScheduling().IsInSchedule())
+		{
+			return false;
+		}
+		
+		if( (GetMOESettings().IsDoorRaidOnlyEnabled || explosive.CanOnlyRaidDoors()) && !baseBuildingBase.HasGate_MOE())
+		{
+			return false;		
+		}
+		
+		return true;
 	}
 	
 	//True = custom damage handled, don't deal vanilla damage 
